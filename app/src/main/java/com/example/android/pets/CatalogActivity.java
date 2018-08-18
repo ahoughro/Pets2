319e9fcbad852c1a3cbdff4351922ffc1c49f8e9
amelia <ladyames@yahoo.com>
2018-08-18 13:28:47 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -0,0 +1,195 @@
+package com.example.android.pets;
+
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Intent;
+
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.ListView;
+
+import com.example.android.pets.data.PetContract.PetEntry;
+import com.example.android.pets.data.PetDbHelper;
+import com.example.android.pets.data.PetProvider;
+
+/**
+ * Displays list of pets that were entered and stored in the app.
+ */
+public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    /** Tag for the log messages */
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    private static final int PET_LOADER = 0;
+
+    //Adapter for the ListView
+    PetCursorAdapter mCursorAdapter;
+
+   // private PetDbHelper mDbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                Log.e(LOG_TAG, "In Catalog Activity in the onCreate for setting up FAB before passing intent ");
+                startActivity(intent);
+
+                Log.e(LOG_TAG, "In Catalog Activity in the onCreate for setting up FAB AFTER passing intent ");
+            }
+        });
+
+        /**
+         * All for project 2:
+         */
+        //Find the ListView which will be populated with the pet data
+        ListView petListView = (ListView) findViewById(R.id.list);
+
+        //Find and set the empty view on the ListView, so that it only shows when the list has 0 items
+        View emptyView = findViewById(R.id.empty_view);
+        petListView.setEmptyView(emptyView);
+
+        //Setup an adapter to create a list item for each row of the pet data in the cursor
+        //There is no pet data yet (until the loader finishes) so pass in null for the cursor
+        mCursorAdapter = new PetCursorAdapter(this, null);
+        petListView.setAdapter(mCursorAdapter);
+
+
+       petListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+               // Create new intent to go to {@Link EditorActivity}
+               Intent intent = new Intent (CatalogActivity.this, EditorActivity.class);
+
+               // Form the content URI that represents the specific pet clicked on
+               // by appending the "id" (passed as input to this method) onto the
+               // {@link PetEntry#CONTENT_URI}.
+               //For example the URI would be "content://com.example.android.pets/pets/2"
+               //If the pet with ID 2 was clicked on
+               Uri currentPetUri = ContentUris.withAppendedId(PetEntry.CONTENT_URI, id);
+
+               //Set the uri on the data field of the intent
+               intent.setData(currentPetUri);
+
+               Log.e(LOG_TAG, "In Catalog activity the currentPetURI is: " + currentPetUri);
+
+               // Launch the {@link EditorActivity} to display the data for the current pet
+               startActivity(intent);
+               Log.e(LOG_TAG, "In Catalog activity after the START ACTIVITY: ");
+           }
+       });
+
+        //KICKOFF the LOADER - Requried for Project 2
+        getLoaderManager().initLoader(PET_LOADER, null, this);
+
+        Log.e(LOG_TAG, "In Catalog activity after the GetLoaderManager: ");
+    }
+
+
+    /**
+     * Helper method to insert hardcoded pet data into the database. For debugging purposes only.
+     */
+    private void insertPet(){
+        //Set the data repository in write mode
+       // SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        //Create a Content Values Object
+        ContentValues values = new ContentValues();
+
+        //Enter in some dummy pet info to test that calling the DB.insert is working.
+        values.put(PetEntry.COLUMN_PET_NAME, "Toto");
+        values.put(PetEntry.COLUMN_PET_BREED, "Terrier");
+        values.put(PetEntry.COLUMN_PET_GENDER, PetEntry.GENDER_MALE);
+        values.put(PetEntry.COLUMN_PET_WEIGHT, "7");
+
+        //Ensure what is returned is not the -1 value
+        //long newRowID = db.insert(PetEntry.TABLE_NAME, null, values);
+
+        Uri newUri = getContentResolver().insert(PetEntry.CONTENT_URI, values);
+
+        //Log.v("CatalogActivity", "newRowID is: " + newRowID);
+    }
+
+    /**
+     * Helper method to delete all pets in the database.
+     */
+    private void deleteAllPets() {
+        int rowsDeleted = getContentResolver().delete(PetEntry.CONTENT_URI, null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from pet database");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+
+                insertPet();
+                //displayDatabaseInfo(); //REMOVE THIS FOR PROJECT 2
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_all_entries:
+                deleteAllPets();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        //Define a projection that specifies the columns from the table we care about
+        String [] projection = {
+                PetEntry._ID,
+                PetEntry.COLUMN_PET_NAME,
+                PetEntry.COLUMN_PET_BREED };
+
+        Log.e(LOG_TAG, "In Catalog Activity in the Loader<CURSOR> before RETURN ");
+        //This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   //Parent Activity Context
+                PetEntry.CONTENT_URI,           //Provider content URI to query
+                projection,                     //Columns to include in the resulting Cursor
+                null,                  //No selection clause
+                null,               // No selection arguments
+                null);                 // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+        // Update (@Link PetCursorAdapter) with this new cursor containing updated pet data
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
+    }
+}
